cfb555dc88a485eaf3b88f8a84a22a2ac0c9f48b
Tim <qitingliu7@gmail.com>
2021-07-06 21:19:06 -0400

    Style-AddEntry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Platfrom
+} from "react-native";
 import { getMetricMetaInfo, timeToString } from "../utils/helpers";
 import produce from "immer";
 import UdaciSlider from "./UdaciSlider";
@@ -10,11 +16,17 @@ import TextButton from "./TextButton";
 import { submitEntry, removeEntry } from "../utils/api";
 import { connect } from "react-redux";
 import { addEntry } from "../actions";
+import { purple, white } from "../utils/colors";
 
 function SubmitBtn({ onPress }) {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>SUBMIT</Text>
+    <TouchableOpacity
+      onPress={onPress}
+      style={
+        Platform.OS === "ios" ? styles.iosSubmitBtn : styles.AndroidSubmitBtn
+      }
+    >
+      <Text style={styles.submitBtnText}>SUBMIT</Text>
     </TouchableOpacity>
   );
 }
@@ -90,10 +102,15 @@ const AddEntry = props => {
   const metaInfo = getMetricMetaInfo();
   if (props.alreadyLogged) {
     return (
-      <View>
+      <View style={styles.center}>
         <Text>You already logged your information for today.</Text>
-        <Ionicons name={"ios-happy"} size={100} />
-        <TextButton onPress={this.reset}>Reset</TextButton>
+        <Ionicons
+          name={Platform.OS === "ios" ? "ios-happy-outline" : "md-happy"}
+          size={100}
+        />
+        <TextButton style={{ padding: 10 }} onPress={this.reset}>
+          Reset
+        </TextButton>
       </View>
     );
   }
@@ -101,11 +118,9 @@ const AddEntry = props => {
   return (
     <View>
       <DateHeader date={new Date().toLocaleDateString()} />
-      <Text>{JSON.stringify(state)}</Text>
       {Object.keys(metaInfo).map(key => {
         const { getIcon, type, ...rest } = metaInfo[key];
         let value = state[key];
-
         return (
           <View key={key}>
             {getIcon()}
@@ -130,6 +145,50 @@ const AddEntry = props => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center"
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  AndroidSubmitBtn: {
+    backgroundColor: purple,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: "flex-end",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: "center"
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30
+  }
+});
 
 function mapStateToProps(state) {
   const key = timeToString();
