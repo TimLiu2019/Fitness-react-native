917c74759309e39f61c204580bb5c8a2eebec93e
Tim <qitingliu7@gmail.com>
2021-07-09 16:00:23 -0400

    Changes for History-MetricCard

--- components/History.js
+++ components/History.js
@@ -13,6 +13,7 @@ import { fetchCalendarResults } from "../utils/api";
 import UdaciFitnessCalendar from "udacifitness-calendar";
 import { white } from "../utils/colors";
 import DateHeader from "./DateHeader";
+import MetricCard from "./MetricCard";
 
 const History = props => {
   const { dispatch } = props;
@@ -40,7 +41,7 @@ const History = props => {
         </View>
       ) : (
         <TouchableOpacity onPress={() => console.log("Pressed!")}>
-          <Text>{JSON.stringify(metrics)}</Text>
+          <MetricCard date={formattedDate} metrics={metrics} />
         </TouchableOpacity>
       )}
     </View>
