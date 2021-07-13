48a53f9100b7364773f8e42d38302d7544e1a142
Tim <qitingliu7@gmail.com>
2021-07-13 08:52:55 -0400

    Changes for navigationOptions

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text } from "react-native";
 
 const EntryDetail = props => {
+  useEffect(() => {
+    const { entryId } = props.route.params;
+    setTitle(entryId);
+  }, []);
+
+  setTitle = entryId => {
+    if (!entryId) return;
+
+    const year = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day = entryId.slice(8);
+
+    props.navigation.setOptions({
+      title: `${month}/${day}/${year}`
+    });
+  };
+
   return (
     <View>
       <Text>Entry Detail - {JSON.stringify(props.route.params.entryId)}</Text>
